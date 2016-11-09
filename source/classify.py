730b34fb5e3592a2a07eff256f9cb2c721217c09
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-11-09 21:34:49 +0900

    add classification code

---
+++ source/classify.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
+
+from dataset import Dataset, Datasets
+
+import os
+import sys
+import pickle
+
+
+# load data
+height = pickle.load(open('height.pkl', 'rb'))
+
+
+# network parameters
+input_dim = 1 # height data input
+encoder_hidden_dim = 64
+decoder_hidden_dim = 64
+latent_dim = 1
+lam = 0 # lambda
+
+
+# define weight shape
+weights = {
+    "encoder_h1": tf.Variable(tf.zeros([input_dim, encoder_hidden_dim])),
+    "encoder_mu": tf.Variable(tf.zeros([encoder_hidden_dim, latent_dim])),
+    "encoder_logvar": tf.Variable(tf.zeros([encoder_hidden_dim, latent_dim])),
+    "decoder_h1": tf.Variable(tf.zeros([latent_dim, decoder_hidden_dim])),
+    "decoder_reconstruction": tf.Variable(tf.zeros([decoder_hidden_dim, input_dim]))
+}
+
+
+# define bias shape
+biases = {
+    "encoder_h1": tf.Variable(tf.zeros([encoder_hidden_dim])),
+    "encoder_mu": tf.Variable(tf.zeros([latent_dim])),
+    "encoder_logvar": tf.Variable(tf.zeros([latent_dim])),
+    "decoder_h1": tf.Variable(tf.zeros([decoder_hidden_dim])),
+    "decoder_reconstruction": tf.Variable(tf.zeros([input_dim]))
+}
+
+
+# encoder
+## define input shape
+x = tf.placeholder(tf.float32, [None, input_dim])
+
+## encoder_h1
+l2_loss = tf.constant(0.0) # l2 norm
+l2_loss += tf.nn.l2_loss(weights["encoder_h1"])
+hidden_encoder = tf.nn.relu(tf.matmul(x, weights["encoder_h1"]) + biases["encoder_h1"])
+
+## encoder_mu
+l2_loss += tf.nn.l2_loss(weights["encoder_mu"])
+mu_encoder = tf.matmul(hidden_encoder, weights["encoder_mu"]) + biases["encoder_mu"]
+
+## encoder_logvar
+l2_loss += tf.nn.l2_loss(weights["encoder_logvar"])
+logvar_encoder = tf.matmul(hidden_encoder, weights["encoder_logvar"]) + biases["encoder_logvar"]
+
+
+# sampling
+## sample epsilon
+epsilon = tf.random_normal(tf.shape(logvar_encoder), name='epsilon')
+
+## sample latent variable
+std_encoder = tf.exp(0.5 * logvar_encoder)
+z = mu_encoder + tf.mul(std_encoder, epsilon)
+
+
+# decoder
+## decoder_h1
+l2_loss += tf.nn.l2_loss(weights["decoder_h1"])
+hidden_decoder = tf.nn.relu(tf.matmul(z, weights["decoder_h1"]) + biases["decoder_h1"])
+
+## decoder_reconstruction
+l2_loss += tf.nn.l2_loss(weights["decoder_reconstruction"])
+x_hat = tf.matmul(hidden_decoder, weights["decoder_reconstruction"]) + biases["decoder_reconstruction"]
+
+
+# get latent representation over test set
+batch_size = 50
+train_iter = int(height.train.num_examples / batch_size)
+test_iter = int(height.test.num_examples / batch_size)
+train = np.zeros((train_iter * batch_size, 3))
+test = np.zeros((test_iter * batch_size, 3))
+
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state('out_models/')
+    if ckpt:
+        last_model = ckpt.model_checkpoint_path
+        saver.restore(sess, last_model)
+    else:
+        print("No trained model was found.")
+        sys.exit(0)
+
+    for i in range(train_iter):
+        batch = height.train.next_batch(batch_size)
+        feed_dict = {x: batch[0]}
+        mu, logvar = sess.run([mu_encoder, logvar_encoder], feed_dict=feed_dict)
+
+        _train = np.concatenate((mu, logvar, batch[1]), axis=1)
+
+        train[batch_size*i:batch_size*(i+1)] = _train
+
+    for i in range(test_iter):
+        batch = height.test.next_batch(batch_size)
+        feed_dict = {x: batch[0]}
+        mu, logvar = sess.run([mu_encoder, logvar_encoder], feed_dict=feed_dict)
+
+        _test = np.concatenate((mu, logvar, batch[1]), axis=1)
+
+        test[batch_size*i:batch_size*(i+1)] = _test
+
+
+# classification
+estimator = LinearSVC(C=100.0)
+estimator.fit(train[:,:-1], train[:,-1].astype(np.int32))
+predictions = estimator.predict(test[:,:-1])
+
+# evaluation
+print("# Evaluation")
+print(" - Male: 0")
+print(" - Female: 1")
+print(classification_report(test[:,-1], predictions))
--- source/vae.py
+++ source/vae.py
@@ -11,17 +11,12 @@ import pickle
 # load data
 height = pickle.load(open('height.pkl', 'rb'))
 
-# parameters
-n_epoch = 20
-batch_size = 50
-display_step = 1
-
 # network parameters
 input_dim = 1 # height data input
 encoder_hidden_dim = 64
 decoder_hidden_dim = 64
-latent_dim = 16
-lam = 0 # lambda
+latent_dim = 1
+lam = 0.0 # lambda
 
 # define weight shape
 weights = {
@@ -48,15 +43,15 @@ x = tf.placeholder(tf.float32, [None, input_dim])
 ## encoder_h1
 l2_loss = tf.constant(0.0) # l2 norm
 l2_loss += tf.nn.l2_loss(weights["encoder_h1"])
-hidden_encoder = tf.nn.relu(tf.matmul(x, weights["encoder_h1"]) + biases["encoder_h1"])
+hidden_encoder = tf.nn.relu(tf.add(tf.matmul(x, weights["encoder_h1"]), biases["encoder_h1"]))
 
 ## encoder_mu
 l2_loss += tf.nn.l2_loss(weights["encoder_mu"])
-mu_encoder = tf.matmul(hidden_encoder, weights["encoder_mu"]) + biases["encoder_mu"]
+mu_encoder = tf.add(tf.matmul(hidden_encoder, weights["encoder_mu"]), biases["encoder_mu"])
 
 ## encoder_logvar
 l2_loss += tf.nn.l2_loss(weights["encoder_logvar"])
-logvar_encoder = tf.matmul(hidden_encoder, weights["encoder_logvar"]) + biases["encoder_logvar"]
+logvar_encoder = tf.add(tf.matmul(hidden_encoder, weights["encoder_logvar"]), biases["encoder_logvar"])
 
 
 # sampling
@@ -64,27 +59,27 @@ logvar_encoder = tf.matmul(hidden_encoder, weights["encoder_logvar"]) + biases["
 epsilon = tf.random_normal(tf.shape(logvar_encoder), name='epsilon')
 
 ## sample latent variable
-std_encoder = tf.exp(0.5 * logvar_encoder)
-z = mu_encoder + tf.mul(std_encoder, epsilon)
+std_encoder = tf.exp(tf.mul(0.5, logvar_encoder))
+z = tf.add(mu_encoder, tf.mul(std_encoder, epsilon))
 
 
 # decoder
 ## decoder_h1
 l2_loss += tf.nn.l2_loss(weights["decoder_h1"])
-hidden_decoder = tf.nn.relu(tf.matmul(z, weights["decoder_h1"]) + biases["decoder_h1"])
+hidden_decoder = tf.nn.relu(tf.add(tf.matmul(z, weights["decoder_h1"]), biases["decoder_h1"]))
 
 ## decoder_reconstruction
 l2_loss += tf.nn.l2_loss(weights["decoder_reconstruction"])
-x_hat = tf.matmul(hidden_decoder, weights["decoder_reconstruction"]) + biases["decoder_reconstruction"]
+x_hat = tf.add(tf.matmul(hidden_decoder, weights["decoder_reconstruction"]), biases["decoder_reconstruction"])
 
 # calculate loss
 kl_divergence = -0.5 * tf.reduce_sum(1 + logvar_encoder - tf.pow(mu_encoder, 2) - tf.exp(logvar_encoder), reduction_indices=1)
 bce = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(x_hat, x), reduction_indices=1)
-loss = tf.reduce_mean(kl_divergence + bce)
-regularized_loss = loss + lam * l2_loss
+loss = tf.reduce_mean(tf.add(kl_divergence, bce))
+regularized_loss = tf.add(loss, tf.mul(lam, l2_loss))
 
 # optimization
-train_step = tf.train.AdamOptimizer(0.01).minimize(regularized_loss)
+train_step = tf.train.AdamOptimizer(0.001).minimize(regularized_loss)
 
 # logging
 loss_summary = tf.scalar_summary("lower_bound", loss)
@@ -93,17 +88,14 @@ saver = tf.train.Saver()
 
 
 # training
-n_epoch = 1000
+n_epoch = 100
 batch_size = 50
 display_step = 1
 
 with tf.Session() as sess:
     summary_writer = tf.train.SummaryWriter('experiment', graph=sess.graph)
 
-    if os.path.isfile("out_models/model.ckpt"):
-        saver.restore(sess, "out_models/model.ckpt")
-    else:
-        sess.run(tf.initialize_all_variables())
+    sess.run(tf.initialize_all_variables())
 
     for epoch in range(1, n_epoch+1):
         batch = height.train.next_batch(batch_size)
@@ -111,6 +103,6 @@ with tf.Session() as sess:
         _, current_loss, summary_str = sess.run([train_step, loss, summary_op], feed_dict=feed_dict)
         summary_writer.add_summary(summary_str, epoch)
 
-        print("# epoch{0:2d}: loss={1:.2f}".format(epoch, current_loss))
+        print("# epoch{0:5d}: loss={1:.5f}".format(epoch, current_loss))
 
     save_path = saver.save(sess, "out_models/model.ckpt")
